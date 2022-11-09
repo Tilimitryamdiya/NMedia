a4fdf924d795624237fa615630deabb4d126de04
Tilimitryamdiya <dawwa@mail.ru>
2022-11-09 20:35:35 +0300

    Added saving text on back button pressed

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -1,12 +1,16 @@
 package ru.netology.nmedia.activity
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_new_post.*
 import ru.netology.nmedia.databinding.FragmentNewPostBinding
 import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.util.StringArg
@@ -16,6 +20,22 @@ class NewPostFragment : Fragment() {
 
     private val viewModel by viewModels<PostViewModel>(ownerProducer = ::requireParentFragment)
     private lateinit var binding: FragmentNewPostBinding
+    private val preferences by lazy { this.activity?.getPreferences(Context.MODE_PRIVATE) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            preferences?.edit()?.apply {
+                if (arguments?.textArg == edit.text.toString()) {
+                    return@apply
+                } else {
+                    putString(SAVE_KEY, edit.text.toString())
+                    apply()
+                }
+            }
+            findNavController().navigateUp()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +50,8 @@ class NewPostFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.edit.requestFocus()
-        arguments?.textArg?.let(binding.edit::setText)
+        arguments?.textArg?.let(binding.edit::setText) ?: preferences?.getString(SAVE_KEY, "")
+            .let(binding.edit::setText)
 
         binding.ok.setOnClickListener {
             val text = binding.edit.text.toString()
@@ -38,12 +59,17 @@ class NewPostFragment : Fragment() {
                 viewModel.changeContent(text)
                 viewModel.save()
             }
+            preferences?.edit {
+                clear()
+                commit()
+            }
             AndroidUtils.hideKeyboard(requireView())
             findNavController().navigateUp()
         }
     }
 
     companion object {
+        const val SAVE_KEY = "save"
         var Bundle.textArg by StringArg
     }
 }
